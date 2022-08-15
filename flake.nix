dfcb1c22074ac9abbbc52a08fcef8ecaba284882
tnichols217 <62992267+tnichols217@users.noreply.github.com>
2022-08-15 09:44:26 +0800

    add flake shell

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "Dev shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          python310
+          python310Packages.cairosvg
+          python310Packages.pypdf2
+          python310Packages.qrcode
+          python310Packages.python-barcode
+          python310Packages.Wand
+        ];
+        buildInputs = [ ];
+      };
+    });
+}
